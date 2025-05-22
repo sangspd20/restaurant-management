720a5d7c2a9bff18490c209a6debdc2987135717
F88\sangnt3 <sangnt3@f88.vn>
2025-05-22 15:13:38 +0700

    5.1_create_env_file

---
+++ config.ts
@@ -0,0 +1,21 @@
+import { z } from "zod";
+
+const configSchema = z.object({
+  NEXT_PUBLIC_API_ENDPOINT: z.string(),
+  NEXT_PUBLIC_URL: z.string(),
+});
+
+const result = configSchema.safeParse({
+  NEXT_PUBLIC_API_ENDPOINT: process.env.NEXT_PUBLIC_API_ENDPOINT,
+  NEXT_PUBLIC_URL: process.env.NEXT_PUBLIC_URL,
+});
+
+if (!result.success) {
+  console.error(result.error);
+  throw new Error("Invalid environment variables");
+} else {
+  console.log(result.data); // typed and validated!
+}
+
+const envConfig = result.data;
+export default envConfig;
