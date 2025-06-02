d65e1983ec83efb2211b0d67b55e2033e7aa5345
F88\sangnt3 <sangnt3@f88.vn>
2025-06-02 16:11:10 +0700

    5.4_login_route_handler

---
+++ src/app/api/auth/login/route.ts
@@ -0,0 +1,44 @@
+import authApiRequests from "@/apiRequests/auth";
+import { LoginBodyType } from "@/schemaValidations/auth.schema";
+import { cookies } from "next/headers";
+import jwt from "jsonwebtoken";
+import { HttpError } from "@/lib/http";
+
+export async function POST(request: Request) {
+  const body = (await request.json()) as LoginBodyType;
+  const cookieStore = cookies();
+  try {
+    const { payload } = await authApiRequests.sLogin(body);
+    const { accessToken, refreshToken } = payload.data;
+
+    const decodedAccessToken = jwt.decode(accessToken) as { exp: number };
+    const decodedRefreshToken = jwt.decode(refreshToken) as { exp: number };
+
+    cookieStore.set("accessToken", accessToken, {
+      path: "/",
+      httpOnly: true,
+      sameSite: "lax",
+      secure: true,
+      expires: decodedAccessToken.exp * 1000,
+    });
+
+    cookieStore.set("refreshToken", refreshToken, {
+      path: "/",
+      httpOnly: true,
+      sameSite: "lax",
+      secure: true,
+      expires: decodedRefreshToken.exp * 1000,
+    });
+
+    return Response.json(payload, { status: 200 });
+  } catch (error) {
+    if (error instanceof HttpError) {
+      return Response.json(error.payload, { status: error.status });
+    } else {
+      return Response.json(
+        { message: "Internal server error" },
+        { status: 500 }
+      );
+    }
+  }
+}
