78589173fec3d2626935967c50e2c42360306bb3
F88\sangnt3 <sangnt3@f88.vn>
2025-06-05 14:39:46 +0700

    5.8 middleware to redirect

---
+++ src/middleware.ts
@@ -0,0 +1,27 @@
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+
+const privatePaths = ["/manage"];
+const unAuthPaths = ["/login"];
+// This function can be marked `async` if using `await` inside
+export function middleware(request: NextRequest) {
+  const { pathname } = request.nextUrl;
+  const isAuth = Boolean(request.cookies.get("accessToken")?.value);
+
+  console.log("isAuth", isAuth);
+  // not login
+  if (privatePaths.some((path) => pathname.startsWith(path)) && !isAuth) {
+    return NextResponse.redirect(new URL("/login", request.url));
+  }
+
+  // logged
+  if (unAuthPaths.some((path) => pathname.startsWith(path)) && isAuth) {
+    return NextResponse.redirect(new URL("/", request.url));
+  }
+  return NextResponse.next();
+}
+
+// See "Matching Paths" below to learn more
+export const config = {
+  matcher: ["/manage/:path*", "/login"],
+};
